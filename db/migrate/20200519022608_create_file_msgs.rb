f0ccf9c43f96cd00aef2b16ee9dff44bb312fc98
qiao_1998 <13014721444@163.com>
2020-05-19 10:32:26 +0800

    fix

--- db/migrate/20200519022608_create_file_msgs.rb
+++ db/migrate/20200519022608_create_file_msgs.rb
@@ -1,7 +1,8 @@
 class CreateFileMsgs < ActiveRecord::Migration[5.2]
   def change
     create_table :file_msgs do |t|
-
+      t.string "file_url", limit: 255
+      t.text "file_info"
       t.timestamps
     end
   end
